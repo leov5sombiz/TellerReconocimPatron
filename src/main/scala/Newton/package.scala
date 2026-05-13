d513e21744aeb6b237f815b1a224017f73589b5d
Leandro Moreno Castillo <elbert.moreno@correounivall.edu.co>
2026-05-13 05:45:50 -0500

    sisisiis

--- src/main/scala/Newton/package.scala
+++ src/main/scala/Newton/package.scala
@@ -10,6 +10,11 @@ package object Newton {
   case class Expo(e1: Expr, e2: Expr) extends Expr
   case class Logaritmo(e1: Expr) extends Expr
 
+  /**
+   * Ejercicio #1: Función Mostrar
+   * @param e: Expresión ya sea númerica o variable
+   * @return Convierte el parámetro en String para mostrarlo en la consola
+   */
   def mostrar(e: Expr): String = e match {
 
     case Numero(d) =>
@@ -36,4 +41,226 @@ package object Newton {
     case Logaritmo(a) =>
       "(lg(" + mostrar(a) + "))"
   }
+
+  /**
+   * Ejercicio #2: Función Derivar
+   * @param f: Expresión a derivar
+   * @param a: Variable respecto a la cual se deriva la expresión
+   * @return Expresión correspondiente a la derivada aplicando reglas de derivación
+   */
+  def derivar(f: Expr, a: Atomo): Expr = f match {
+
+    case Numero(_) =>
+      Numero(0)
+
+    case Atomo(x) =>
+      if (x == a.x) Numero(1)
+      else Numero(0)
+
+    case Suma(e1, e2) =>
+      Suma(derivar(e1, a), derivar(e2, a))
+
+    case Resta(e1, e2) =>
+      Resta(derivar(e1, a), derivar(e2, a))
+
+    case Prod(e1, e2) =>
+      Suma(
+        Prod(derivar(e1, a), e2),
+        Prod(e1, derivar(e2, a))
+      )
+
+    case Div(e1, e2) =>
+      Div(
+        Resta(
+          Prod(derivar(e1, a), e2),
+          Prod(e1, derivar(e2, a))
+        ),
+        Expo(e2, Numero(2))
+      )
+
+    case Logaritmo(e) =>
+      Div(derivar(e, a), e)
+
+    case Expo(e1, e2) =>
+      Prod(
+        Expo(e1, e2),
+        Suma(
+          Div(
+            Prod(derivar(e1, a), e2),
+            e1
+          ),
+          Prod(
+            derivar(e2, a),
+            Logaritmo(e1)
+          )
+        )
+      )
+  }
+
+  /**
+   * Ejercicio #3: Función Evaluar
+   * @param f: Expresión matemática a evaluar
+   * @param a: Variable
+   * @param v: Valor con el que se evalúa la expresión
+   * @return Imagen de la función evaluada en "v"
+   */
+  def evaluar(f: Expr, a: Atomo, v: Double): Double = f match {
+
+    case Numero(d) =>
+      d
+
+    case Atomo(x) =>
+      v
+
+    case Suma(e1, e2) =>
+      evaluar(e1, a, v) +
+        evaluar(e2, a, v)
+
+    case Prod(e1, e2) =>
+      evaluar(e1, a, v) *
+        evaluar(e2, a, v)
+
+    case Resta(e1, e2) =>
+      evaluar(e1, a, v) -
+        evaluar(e2, a, v)
+
+    case Div(e1, e2) =>
+      evaluar(e1, a, v) /
+        evaluar(e2, a, v)
+
+    case Expo(e1, e2) =>
+      Math.pow(
+        evaluar(e1, a, v),
+        evaluar(e2, a, v)
+      )
+
+    case Logaritmo(e) =>
+      Math.log(
+        evaluar(e, a, v)
+      )
+  }
+
+  /**
+   * Ejercicio #4: Función Limpiar
+   * @param f: Expresión matemática a limpiar
+   * @return Expresión simplificada de vforma que se vea más prolija
+   */
+  def limpiar(f: Expr): Expr = f match {
+
+    case Numero(d) =>
+      Numero(d)
+
+    case Atomo(x) =>
+      Atomo(x)
+
+    case Suma(e1, e2) =>
+
+      (limpiar(e1), limpiar(e2)) match {
+
+        case (Numero(0), b) =>
+          b
+
+        case (a, Numero(0)) =>
+          a
+
+        case (a, b) =>
+          Suma(a, b)
+      }
+
+    case Resta(e1, e2) =>
+
+      (limpiar(e1), limpiar(e2)) match {
+
+        case (a, Numero(0)) =>
+          a
+
+        case (a, b) =>
+          Resta(a, b)
+      }
+
+    case Prod(e1, e2) =>
+
+      (limpiar(e1), limpiar(e2)) match {
+
+        case (Numero(0), _) =>
+          Numero(0)
+
+        case (_, Numero(0)) =>
+          Numero(0)
+
+        case (Numero(1), b) =>
+          b
+
+        case (a, Numero(1)) =>
+          a
+
+        case (a, b) =>
+          Prod(a, b)
+      }
+
+    case Div(e1, e2) =>
+
+      (limpiar(e1), limpiar(e2)) match {
+
+        case (Numero(0), _) =>
+          Numero(0)
+
+        case (a, Numero(1)) =>
+          a
+
+        case (a, b) =>
+          Div(a, b)
+      }
+
+    case Expo(e1, e2) =>
+
+      (limpiar(e1), limpiar(e2)) match {
+
+        case (_, Numero(0)) =>
+          Numero(1)
+
+        case (a, Numero(1)) =>
+          a
+
+        case (a, b) =>
+          Expo(a, b)
+      }
+
+    case Logaritmo(e) =>
+      Logaritmo(limpiar(e))
+  }
+
+  /**
+   * Ejercicio #5: Función raizNewton
+   * @param f: Expresión a sacar raíces
+   * @param a: Variable a tener en cuenta
+   * @param x0: Aproximación inicial
+   * @param ba: Indica si ya se está lo suficientemente cerca de la raíz
+   * @return
+   */
+
+  def raizNewton(
+                  f: Expr,
+                  a: Atomo,
+                  x0: Double,
+                  ba: (Expr, Atomo, Double) => Boolean
+                ): Double = {
+
+    if (ba(f, a, x0))
+      x0
+
+    else {
+
+      val df = derivar(f, a)
+
+      val x1 =
+        x0 - (
+          evaluar(f, a, x0) /
+            evaluar(df, a, x0)
+          )
+
+      raizNewton(f, a, x1, ba)
+    }
+  }
+
 }
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,13 +1,70 @@
-import Newton._
+  import Newton._
 
-val expr1 = Numero(2)
-val expr2 = Numero(4)
-val expr3 = Suma(expr1,expr2)
-val expr4 = Resta(expr1,expr2)
-val expr5 = Prod(expr1, expr2)
+  val expr1 = Suma(Atomo('x'), Numero(2))
+  val expr2 = Prod(Atomo('x'), Atomo('x'))
+  val expr3 = Logaritmo(Atomo('x'))
+  val expr4 = Expo(Atomo('x'), Numero(3))
+  val expr5 = Suma(expr1, Expo(expr2, Numero(5)))
+  val expr6 = Div(expr2, Atomo('x'))
+  val expr7 = Prod(Atomo('y'), Atomo('y'))
+  val expr8 = Resta(Numero(10), expr2)
 
-mostrar(expr1)
-mostrar(expr2)
-mostrar(expr3)
-mostrar(expr4)
-mostrar(expr5)
+
+  mostrar(expr1)
+  mostrar(expr2)
+  mostrar(expr3)
+  mostrar(expr4)
+  mostrar(expr5)
+  mostrar(expr6)
+  mostrar(expr7)
+  mostrar(expr8)
+
+
+  mostrar(derivar(expr1,Atomo('x')))
+  mostrar(derivar(expr2,Atomo('x')))
+  mostrar(derivar(expr3,Atomo('x')))
+  mostrar(derivar(expr4,Atomo('x')))
+  mostrar(derivar(expr6,Atomo('x')))
+  mostrar(derivar(expr7,Atomo('y')))
+
+
+  mostrar(Numero(5.0))
+  evaluar(Numero(5.0), Atomo('x'), 1.0)
+
+  mostrar(Atomo('x'))
+  evaluar(Atomo('x'), Atomo('x'), 5.0)
+
+  mostrar(Suma(expr1, expr2))
+  evaluar(Suma(expr1, expr2), Atomo('x'), 5.0)
+
+  mostrar(Prod(expr1, expr2))
+  evaluar(Prod(expr1, expr2), Atomo('x'), 5.0)
+
+  mostrar(Resta(expr1, expr2))
+  evaluar(Resta(expr1, expr2), Atomo('x'), 5.0)
+
+
+  limpiar(derivar(expr1,Atomo('x')))
+  mostrar(limpiar(derivar(expr1,Atomo('x'))))
+
+  limpiar(derivar(expr2,Atomo('x')))
+  mostrar(limpiar(derivar(expr2,Atomo('x'))))
+
+  limpiar(derivar(expr4,Atomo('x')))
+  mostrar(limpiar(derivar(expr4,Atomo('x'))))
+
+  limpiar(derivar(expr6,Atomo('x')))
+  mostrar(limpiar(derivar(expr6,Atomo('x'))))
+
+  limpiar(derivar(expr7,Atomo('y')))
+  mostrar(limpiar(derivar(expr7,Atomo('y'))))
+
+
+  def buenaAprox(f: Expr, a: Atomo, d: Double): Boolean = {
+    Math.abs(evaluar(f, a, d)) < 0.0001}
+
+  raizNewton(expr1, Atomo('x'), 2.0, buenaAprox)
+  raizNewton(expr2, Atomo('x'), 2.0, buenaAprox)
+  raizNewton(expr3, Atomo('x'), 2.0, buenaAprox)
+  raizNewton(expr4, Atomo('x'), 2.0, buenaAprox)
+  raizNewton(expr8, Atomo('x'), 3.0, buenaAprox)
